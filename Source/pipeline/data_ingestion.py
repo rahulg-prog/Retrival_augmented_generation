1ceace1fb01453aae380c4bdab3aa7c6c097dad8
rahulg-prog <rahul.g@webknot.in>
2025-12-24 14:23:14 +0530

    Add configuration, data ingestion, and initial notebook setup

---
+++ Source/pipeline/data_ingestion.py
@@ -0,0 +1,14 @@
+import sys
+from pathlib import Path
+
+current_file = Path(__file__).resolve()
+project_root = current_file.parent.parent
+sys.path.insert(0, str(project_root))
+
+from logger.custom_logger import CustomLogger
+from config.settings_loader import load_config
+
+from abc import ABC, abstractmethod
+from dotenv import load_dotenv
+
+load_dotenv()
